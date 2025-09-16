c5a66a76a75dc180c2f4bc0aef374efe323cd877
jesusvargasc <estivenvargas0412@gmail.com>
2025-09-16 14:31:04 +0000

    Agreggo correccion de ejercicio 4_1, donde se utiliza ahora si vector.

--- ejercicio4_1.cpp
+++ ejercicio4_1.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 
-int main() {  
-    int x, y, s, j, z;
-    int aux;
-    std::cout << "Ingrese el primer digito: ";
-    std::cin >> x;
-    std::cout << "Ingrese el segundo digito: ";
-    std::cin >> y;
-    std::cout << "Ingrese el tercer digito: ";
-    std::cin >> s;
-    std::cout << "Ingrese el cuarto digito: ";
-    std::cin >> j;
-    std::cout << "Ingrese el quinto digito: ";
-    std::cin >> z;
-    aux = x + y + s + j + z;
-    std::cout << "La suma de todas las variables es: " << aux << std::endl;
+int main() {
+    int arreglo[5] = {0, 0, 0, 0, 0};
+    int suma = 0;
+    int n=0;
+
+    for (int i = 0; n < 5; i++) {
+        std::cout << "Ingrese el elemento " << (i + 1) << ": ";
+        std::cin >> arreglo[i]; 
+        suma += arreglo[i];
+        n++;
+    }
+
+    std::cout << "La suma de los elementos es: " << suma << std::endl;
+
     return 0;
 }
