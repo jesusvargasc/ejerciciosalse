b661db5687b1b5ed8dd53a7cc7229b6c10e12398
jesusvargasc <estivenvargas0412@gmail.com>
2025-09-19 02:08:35 +0000

    Agrego primer avance de punto 9, solicita no mas los numeros

---
+++ ejercicio9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    std::cout<<"Programa que calcula MCD, por favor poner números enteres y pequeños";
+    int j,s;
+    std::cout<<"Ingrese el primer número:";
+    std::cin>>j;
+    std::cout<<"Ingrese el segundo número:";
+    std::cin>>s;
+
+    
+}
